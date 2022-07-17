f2b73a6ae04f47f5559a5884a80728f2cfae9723
stephanieginting <sag292@cornell.edu>
2022-07-17 11:02:33 -0400

    fix variable and function names

--- src/main/java/com/google/sps/data/Spots.java
+++ src/main/java/com/google/sps/data/Spots.java
@@ -51,7 +51,8 @@ class StudySpot{
 
 class StudySpots implements Iterable<StudySpot> {
   """
-  Class for all the study spots
+  Class for all the study spots from all the colleges 
+  i.e. every singles study spot in the JSON
   """
   List<StudySpot> studyspots;
 
@@ -59,7 +60,7 @@ class StudySpots implements Iterable<StudySpot> {
     return studyspots;
   }
 
-  public void setHours(List<StudySpot> studyspots) {
+  public void setStudySpots(List<StudySpot> studyspots) {
     this.studyspots = studyspots;
   }
 
@@ -70,20 +71,20 @@ class StudySpots implements Iterable<StudySpot> {
 
 }
 
-class Utils {
+class studySpotUtils {
   """
   Class for handling studyspot objects
   """
   HashMap<String, StudySpot> studyspots;
 
-  HashMap<String, StudySpot> convert_to_hashmap(StudySpots studyspots) {
-    HashMap<String, StudySpot> studyspots_hashmap = new HashMap<>();
+  HashMap<String, StudySpot> convertToHashmap(StudySpots studyspots) {
+    HashMap<String, StudySpot> studyspotsHashmap = new HashMap<>();
     
     for (spot : studyspots) {
-      studyspots_hashmap.put(spot.getName(), spot)
+      studyspotsHashmap.put(spot.getName(), spot)
     }
 
-    return studyspots_hashmap;
+    return studyspotsHashmap;
   }
 
   public static void main(String[] args) {
@@ -93,18 +94,18 @@ class Utils {
         try (Reader reader = new FileReader(source)) {
 
           // Convert JSON File to Java Object
-          StudySpots studyspots_from_json = gson.fromJson(reader, StudySpots.class);
+          StudySpots studyspotsFromJson = gson.fromJson(reader, StudySpots.class);
 			
 			    // set studyspots
           // convert to hashmap for easier retrieval later on
-          this.studyspots = convert_to_hashmap(studyspots_from_json);
+          this.studyspots = convert_to_hashmap(studyspotsFromJson);
 
         } catch (IOException e) {
           System.out.println("500 Internal Server Error");
         }
     }
 
-  StudySpot get_studyspot_from_name(String name) {
+  StudySpot getStudySpotFromName(String name) {
     """
     Given a name, return the StudySpot object associated with 
     """
