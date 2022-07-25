fc624cf12f694c0be8812be5576756b85522e702
Jenny <jznnylam@gmail.com>
2022-07-25 21:31:32 +0000

    Remove hardcoded colleges pages, add central college page with updated map/study spots, clean up code

--- src/main/webapp/location.js
+++ src/main/webapp/location.js
@@ -1,45 +1,28 @@
 /**
  * Loads locations onto DOM
  */
-const getLocations = async (location) => {
-    console.log("Test")
-    // Fetch data and convert to JenSON
+const getLocations = async (college, map) => {
+
+    if (college != null) {
+        localStorage.setItem('location-college', college);
+        localStorage.setItem('location-map', map);
+    }
+
+    // Fetch data and convert to JSON
     const response = await fetch('/GetLocations');
     const locationList = await response.json();
     const locationSegment = document.getElementById("card-collection");
-    console.log(locationSegment)
-    console.log(locationList)
     
     locationList.forEach((location) => {
-        console.log("YYY")
-        console.log(location)
-        console.log("#"+location.college)
-        console.log(window.location.hash)
-        if("#"+location.college.toUpperCase() == (window.location.hash.toUpperCase()))
-        {
+        if (location.college === localStorage.getItem("location-college")) {
             locationSegment.appendChild(createLocationElement(location));
             locationSegment.appendChild(document.createElement("br"));
         }
-        
     });
-    
-    // Update reviews to display location
-
-    /*
-    // If page is refreshed, location would be null, so use info from local storage and don't set a new one
-    if (location != null) {
-        localStorage.setItem('location', location);
-        console.log("location is not null");
-    }
-    
-    document.getElementById("location-name").textContent = localStorage.getItem('location');
-    */
+    document.getElementById("map").src = localStorage.getItem("location-map");
 }
 
-
-const createLocationElement = (location) =>
-{
-    console.log("EEE");
+const createLocationElement = (location) => {
     const {description, name, time, college} = location;
 
     const card = document.createElement("div" );
@@ -62,20 +45,8 @@ const createLocationElement = (location) =>
     card.appendChild(header);
     card.appendChild(blurb);
     card.appendChild(desc);
-    console.log("LLL")
-    console.log(name)
-    console.log(college)
-    console.log(card);
     return card;
-    
-
-    console.log(location)
 }
-
-function temp(){
-    console.log("Temp")
-}
-
 
 window.onload = getLocations();
 
